508e50968ed27dce1ee3cf833626b4f0136aaee4
jgarcia1300 <91914498+jgarcia1300@users.noreply.github.com>
2021-11-07 04:26:06 -0600

    adding list practice functions

--- python pratice/list_practice.py
+++ python pratice/list_practice.py
@@ -1,31 +1,55 @@
 cities = ["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
-print(cities)
+
 
 horror_movies = ["chucky", "friday the 13th", "nightmare on elm street", "halloween", "scream", "donnie darko", "texas chainsaw", "conjuring", "hellraiser", "it"]
-print(horror_movies)
 
-print(cities[-3], cities[2], cities[-5])
-print(horror_movies[0], horror_movies[-3], horror_movies[-6])
+
+
 
 first_four_cities = cities[0:4]
-print(first_four_cities)
+
 
 last_four_horror_movies = horror_movies[-5:-1]
-print(last_four_horror_movies)
+
 
 cities[0] = "San Fransisco"
 cities[2] = "Brooklyn"
 cities[-3] = "Hollywood"
 cities[-5] = "Tampa"
-print(cities)
+
 
 cities.append("Oakland")
 cities.extend(["New York City" , "Los Angeles"])
 cities.insert(0, "Miami")
-print(cities)
+
 
 del cities[0]
 cities.pop(-1)
 cities.remove("Tampa")
-print(cities)
 
+def print_city(list):
+    for el in list:
+        print(el)
+    return "All Cities Printed"
+print(print_city(cities))
+
+def reorganize_list(list):
+    counter = 0
+    while counter < len(list):
+        item1 = list[counter]
+        item2 = list[counter + 1]
+        if len(item1) >= len(item2):
+            counter += 1
+            continue 
+        elif counter + 1 == len(list) - 1:
+            break 
+        else:
+            list.remove(item1)
+            list.append(item1)
+            counter += 1
+    return list
+print(reorganize_list(cities))
+
+def sort_list(list):
+    return sorted(list)
+print(sort_list(horror_movies))
